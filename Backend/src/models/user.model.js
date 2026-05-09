ff2f1ddf979b9074f4f3a6792ca60cbde9b9e5d8
Shubhanker01 <shubhanker40@gmail.com>
2026-05-09 10:52:07 +0530

    Added prehooks and password validation method

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from "mongoose";
-
+import bcrypt from "bcrypt"
 const userSchema = new Schema({
     avatar: {
         type: {
@@ -10,7 +10,64 @@ const userSchema = new Schema({
             url: 'https://placehold.co/200x200/png',
             localPath: ''
         }
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        index: true,
+        trim: true,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    fullName: {
+        type: String,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+    isEmailVerified: {
+        type: Boolean,
+        default: false
+    },
+    refreshToken: {
+        type: String
+    },
+    forgotPasswordToken: {
+        type: String
+    },
+    forgotPasswordExpiry: {
+        type: Date
+    },
+    emailVerificationToken: {
+        type: String
+    },
+    emailVerificationExpiry: {
+        type: Date
     }
+
+}, {
+    timestamps: true
 })
+
+// defining prehook
+userSchema.pre("save", async function () {
+    // when not changing the password move immediately to next
+    if (!this.isModified("password")) return next()
+    this.password = await bcrypt.hash(this.password)
+    next()
+})
+
+userSchema.methods.passwordValidate = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
 
 export const User = mongoose.model("User", userSchema)
